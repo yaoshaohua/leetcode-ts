0c63407b73cc7119b12e58ae09b40cbc6530b508
yaoshaohua <eurallyao@163.com>
2022-07-05 07:53:53 +0800

    5,77

---
+++ 5. Longest Palindromic Substring.ts
@@ -0,0 +1,38 @@
+/*
+ * @Description: 
+
+  给你一个字符串 s，找到 s 中最长的回文子串。
+  输入：s = "babad"
+  输出："bab"
+  解释："aba" 同样是符合题意的答案。
+
+ * @Author: ysh
+ * @Date: 2022-07-05 05:47:59
+ * @LastEditors: ysh
+ * @LastEditTime: 2022-07-05 07:52:03
+ */
+
+function longestPalindrome(s: string): string {
+  let start = 0;
+  let maxLen = 1;
+
+  function expandAroundCenter(s: string, left: number, right: number): number {
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      left--;
+      right++;
+    }
+    return right - left - 1;
+  }
+
+  for (let i = 0; i < s.length - 1; i++) {
+    let oddLen = expandAroundCenter(s, i, i);
+    let evenLen = expandAroundCenter(s, i, i + 1);
+    let curMaxLen = Math.max(oddLen, evenLen);
+    if (curMaxLen > maxLen) {
+      maxLen = curMaxLen;
+      start = i - Math.floor((maxLen - 1) / 2);
+    }
+  }
+
+  return s.substring(start, start + maxLen);
+}
---
+++ 77. Combinations.ts
@@ -0,0 +1,38 @@
+/*
+ * @Description: 
+
+  给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
+  输入：n = 4, k = 2
+  输出：
+  [
+    [2,4],
+    [3,4],
+    [2,3],
+    [1,2],
+    [1,3],
+    [1,4],
+  ]
+
+ * @Author: ysh
+ * @Date: 2022-07-05 07:19:54
+ * @LastEditors: ysh
+ * @LastEditTime: 2022-07-05 07:22:34
+ */
+
+function combine(n: number, k: number): number[][] {
+  const res: number[][] = [];
+  backtracking();
+  return res;
+  
+  function backtracking(start: number = 1, tempArr: number[] = []) {
+    if (tempArr.length === k) {
+      res.push([...tempArr]);
+      return;
+    }
+    for (let i = start; i <= n; i++) {
+      tempArr.push(i);
+      backtracking(i + 1, tempArr);
+      tempArr.pop();
+    }
+  }
+}
